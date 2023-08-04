9dff843f366f1570e1b737b3b7e4a1fa66193dc1
dsk90it <dsk90it@gmail.com>
2023-08-04 16:44:42 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+
+import getPlayers from './data/get-players'
+import List from './pages/List'
+import Details from './pages/Details'
+
+function App() {
+  const [players, setPlayers] = useState([])
+  const [selectedPlayer, setSelectedPlayer] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const playersData = await getPlayers()
+        setPlayers(playersData)
+      } catch (error) {
+        console.error('Error fetching players:', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <h1>Cricketer List</h1>
+
+      {!selectedPlayer ? (
+        <List items={players} handleClick={() => null} />
+      ) : (
+        <Details player={selectedPlayer} handleBack={() => setSelectedPlayer(null)} />
+      )}
+    </>
+  )
+}
+
+export default App
